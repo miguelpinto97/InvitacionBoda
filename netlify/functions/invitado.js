56d258b5c01725033d0435e45133ea906a8ff816
miguelpinto97 <pintog.ma97@gmail.com>
2025-09-29 20:36:30 -0500

    Ajustes Invitacion + Modal CONfirmacion (Avance)

--- netlify/functions/invitado.js
+++ netlify/functions/invitado.js
@@ -3,47 +3,52 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6Di7CyehlsNI08PGpBHU75VJhVMWxEJs",
-  authDomain: "invitadosboda-e1314.firebaseapp.com",
-  projectId: "invitadosboda-e1314",
-  storageBucket: "invitadosboda-e1314.firebasestorage.app",
-  messagingSenderId: "159250168397",
-  appId: "1:159250168397:web:aa693cd4472ba0bf2f3a04",
-  measurementId: "G-6Y772NVW4S"
+    apiKey: "AIzaSyC6Di7CyehlsNI08PGpBHU75VJhVMWxEJs",
+    authDomain: "invitadosboda-e1314.firebaseapp.com",
+    projectId: "invitadosboda-e1314",
+    storageBucket: "invitadosboda-e1314.firebasestorage.app",
+    messagingSenderId: "159250168397",
+    appId: "1:159250168397:web:aa693cd4472ba0bf2f3a04",
+    measurementId: "G-6Y772NVW4S"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function handler(event) {
-  try {
-    const { Id } = event.queryStringParameters;
+    try {
+        const { Id } = event.queryStringParameters;
 
-    if (!Id) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Falta parámetro 'Id'" })
-      };
+        if (!Id) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Falta parámetro 'Id'" })
+            };
+        }
+
+        const docRef = doc(db, "Invitados", Id);
+        const docSnap = await getDoc(docRef);
+
+
+        if (docSnap.exists()) {
+            const data = docSnap.data();
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    Nombre: data.Nombre,
+                    Cantidad: data.Cantidad
+                })
+            };
+        } else {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ error: "Invitado no encontrado" })
+            };
+        }
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: err.message })
+        };
     }
-
-    const docRef = doc(db, "Invitados", Id);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ id: docSnap.id, ...docSnap.data() })
-      };
-    } else {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: "Invitado no encontrado" })
-      };
-    }
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
-  }
 }
